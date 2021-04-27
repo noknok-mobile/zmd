5aef10c2b36fb52824ce841351956ef66599c6f5
ulngollm <ulngollm@gmail.com>
2021-04-28 01:06:11 +0300

    add svg task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
-const {dest, src, series, parallel, watch} = require('gulp');
+const {
+    dest,
+    src,
+    series,
+    parallel,
+    watch
+} = require('gulp');
 const pug = require('gulp-pug');
 const browserSync = require("browser-sync").create();
 const sass = require('gulp-dart-sass');
-const concat = require('gulp-concat');
-// sass.compiler = require('node-sass');
 const bulkSass = require('gulp-sass-bulk-import');
+const svgSprite = require('gulp-svg-sprite');
 
-function compileHtml(){
+function compileHtml() {
     return src('src/pages/*.pug')
-    .pipe(pug({
-        pretty: true,
-        basedir: '/home/ully/Документы/dev/zimamed/src/'
-      }))
-    .pipe(dest('build/'));
+        .pipe(pug({
+            pretty: true,
+            basedir: '/home/ully/Документы/dev/zimamed/src/'
+        }))
+        .pipe(dest('build/'));
 }
-function server(){
+
+function server() {
     browserSync.init({
         server: {
             baseDir: "./build/"
@@ -22,18 +28,42 @@ function server(){
     });
     watch("build/").on('change', browserSync.reload);
 }
-function styles(){
+
+function styles() {
     return src('src/sass/main.scss')
-    .pipe(bulkSass())
-    .pipe(sass({outputStyle: "expanded"}).on('error', sass.logError))
-    // .pipe(concat('main.css'))
-    .pipe(dest('build/'));
+        .pipe(bulkSass())
+        .pipe(sass({
+            outputStyle: "expanded"
+        }).on('error', sass.logError))
+        .pipe(dest('build/'));
 }
 
-function watcher(){
+function watcher() {
     watch('src/**/*.pug', compileHtml);
-    watch('src/**/*.scss',styles);
+    watch('src/**/*.scss', styles);
+}
+
+function svg() {
+    return src('assets/svg/*.svg')
+        .pipe(svgSprite({
+            svg: { // General options for created SVG files
+                xmlDeclaration: false, // Add XML declaration to SVG sprite
+                doctypeDeclaration: false, // Add DOCTYPE declaration to SVG sprite
+                namespaceIDs: false, // Add namespace token to all IDs in SVG shapes
+                namespaceIDPrefix: '', // Add a prefix to the automatically generated namespaceIDs
+                namespaceClassnames: false, // Add namespace token to all CSS class names in SVG shapes
+                dimensionAttributes: true // Width and height attributes on the sprite
+            },
+            mode: {
+                symbol: {
+                    dest: './',
+                    sprite: 'icons.svg'
+                }
+            },
+        }))
+        .pipe(dest('src/layout/'));
 }
 exports.server = parallel(server, watcher);
 exports.build = parallel(compileHtml, styles);
 exports.styles = series(styles);
+exports.svg = series(svg);
