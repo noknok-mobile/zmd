e93acecdcddb6719aef9c3d22af6bdbffc8d22dd
ulngollm <ulngollm@gmail.com>
2021-07-04 01:12:31 +0300

    add schedule single

---
+++ src/components/schedule.js
@@ -0,0 +1,12 @@
+let labels = {
+    'expanded': 'Свернуть все',
+    'default': 'Развернуть все'
+}
+document.querySelector('.js-expand-trigger').addEventListener('click', function (e) {
+    e.preventDefault();
+    let isExpand = e.currentTarget.dataset.expand;
+    e.currentTarget.dataset.expand = (isExpand) ? "" : "true";
+    e.currentTarget.querySelector('.js-expand-label').innerText = (e.currentTarget.dataset.expand) ? labels['expanded'] : labels['default'];
+
+    document.querySelector('.js-expand-target').classList.toggle('schedule__inner_fold');
+});
