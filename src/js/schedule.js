9c2ce9547c3e8ac9c41bf4bf41d368a255d037ba
ulngollm <ulngollm@gmail.com>
2021-07-13 20:50:25 +0300

    fix

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -2,13 +2,15 @@ let labels = {
     'expanded': 'Свернуть все',
     'default': 'Развернуть все'
 }
-document.querySelector('.js-expand-trigger').addEventListener('click', expandCalendar);
+for (let button of document.querySelectorAll('.js-expand-trigger')) {
+    button.addEventListener('click', expandCalendar);
+}
 
 function expandCalendar(e) {
     e.preventDefault();
     let isExpand = e.currentTarget.dataset.expand;
     e.currentTarget.dataset.expand = (isExpand) ? "" : "true";
-    e.currentTarget.querySelector('.js-expand-label').innerText = (e.currentTarget.dataset.expand) ? labels['expanded'] : labels['default'];
+    e.currentTarget.querySelector('.schedule__more-label').innerText = (e.currentTarget.dataset.expand) ? labels['expanded'] : labels['default'];
 
-    document.querySelector('.js-expand-target').classList.toggle('schedule__inner_fold');
+    e.currentTarget.closest('.schedule').querySelector('.js-expand-target').classList.toggle('schedule__inner_fold');
 }
