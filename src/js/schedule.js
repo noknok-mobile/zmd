7d668c16fd61b39f4a04bea63a93a6b3ed452124
ulngollm <ulngollm@gmail.com>
2021-07-15 16:29:53 +0300

    update calendar

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -10,7 +10,7 @@ function expandCalendar(e) {
     e.preventDefault();
     let isExpand = e.currentTarget.dataset.expand;
     e.currentTarget.dataset.expand = (isExpand) ? "" : "true";
-    e.currentTarget.querySelector('.schedule__more-label').innerText = (e.currentTarget.dataset.expand) ? labels['expanded'] : labels['default'];
+    e.currentTarget.querySelector('.js-expand-label').innerText = (e.currentTarget.dataset.expand) ? labels['expanded'] : labels['default'];
 
     e.currentTarget.closest('.schedule').querySelector('.js-expand-target').classList.toggle('schedule__inner_fold');
 }
