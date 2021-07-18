6d9d0a11b5f975cba71468e77d54d18a680b6f6f
ulngollm <ulngollm@gmail.com>
2021-07-18 20:38:37 +0300

    update password input

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,9 +1,4 @@
-function togglePasswordIcon(e){
-    if(e.target.classList.contains('icon')){
-        const icon = e.target;
-        icon.classList.toggle('input__icon_password-visible')
-        const inputType = e.currentTarget.querySelector('input').type;
-        inputType == 'password'?? 'text';
-    }
-
+function togglePasswordVisibility(e) {
+    const input = e.target.previousSibling;
+    input.type = (input.type == 'password') ? "text" : "password";
 }
--- src/layout/template.js
+++ src/layout/template.js
@@ -2,3 +2,7 @@ const loginTabFilter = new Filter(document.body, {
     controlsClass: "js-modal-filter",
     targetClass: "js-modal-tab"
 });
+
+for(let icon of document.querySelectorAll('.js-password-toggle')){
+    icon.addEventListener('click', togglePasswordVisibility);
+}
